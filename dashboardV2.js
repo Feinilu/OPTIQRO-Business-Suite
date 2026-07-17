1dbd63f9e772732417f60fbc06c8030413f4edae
Feinilu <stephania.neyra@gmail.com>
2026-07-16 21:23:24 -0600

    Dashboard V2 integrado

---
+++ dashboardV2.js
@@ -0,0 +1,7 @@
+document.getElementById("cotizaciones").innerHTML=125;
+
+document.getElementById("clientes").innerHTML=86;
+
+document.getElementById("productos").innerHTML=348;
+
+document.getElementById("compras").innerHTML=19;
